c5ea4b20ddaa1c340548a07aa874797f03263ff8
adiltekin <adiltekin98@gmail.com>
2022-12-06 11:01:00 +0300

    Update ConnectionString

--- ProjectsBlogWebAPI/Program.cs
+++ ProjectsBlogWebAPI/Program.cs
@@ -6,23 +6,30 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(
+        builder =>
+        {
+            builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
+        });
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
 
 builder.Services.AddDbContext<ProjectsBlogDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")
 ));
 
 var app = builder.Build();
 
-app.UseCors(options => options.WithOrigins("http://localhost:3000").AllowAnyMethod().AllowAnyHeader());
+app.UseCors(option => { option.WithOrigins("*").AllowAnyMethod().AllowAnyHeader(); });
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
